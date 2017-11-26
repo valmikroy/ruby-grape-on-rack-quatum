cd5c413e92102b86f87a0cbae9cfffc612e3caa6
valmikroy <valmikroy@gmail.com>
2017-11-25 20:57:32 -0800

    Prilimanary POST example

--- api/particles/matter/fermions.rb
+++ api/particles/matter/fermions.rb
@@ -13,6 +13,13 @@ module Quantum
         ok 200, "#{get_spin} spin"
       end
 
+      # example curl for below endpoint
+      # curl -H "Content-Type: application/json"  -X POST -d '{"key1":"value1", "key2":"value2"}'  127.0.0.1:8080/api/matter/particles/fermions
+      # curl   -X POST -d   "key1=value1&key2=value2"  127.0.0.1:8080/api/matter/particles/fermions
+      post do
+        ok 200, "blah #{params['key1']}   #{params['key2']} "
+      end
+
       $LOAD_PATH.unshift(File.expand_path('../fermions', __FILE__))
       require 'quarks'
       mount Quarks
